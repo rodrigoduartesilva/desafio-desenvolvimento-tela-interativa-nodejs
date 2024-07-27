01239ea1fa71fa6642f3dad519ab5c1e1cd7a94e
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-07-27 12:02:06 -0300

    feat: implementacao da funcao de escolha do sexo do personagem

---
+++ src/funcoes/selecionarSexoDoPersonagem.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+
+function selecionarSexoDoPersonagem() {
+
+    let sexoSelecionado;
+    const sexo = +prompt('Escolhao sexo do seu personagem: 1 - Masculino | 2 - Feminino: ');
+
+    switch (sexo) {
+        case 1:
+            sexoSelecionado = 'Masculino';
+            console.log(`Seu personagem é do sexo ${sexoSelecionado}`);
+            break;
+        case 2:
+            sexoSelecionado = 'Feminino';
+            console.log(`Seu personagem é do sexo ${sexoSelecionado}`);
+            break;
+        default:
+            console.log('\n');
+            console.log('Opção inválida!');
+            break;
+    }
+
+    return sexoSelecionado;
+
+}
+
+module.exports = selecionarSexoDoPersonagem;
